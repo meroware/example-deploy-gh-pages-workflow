09a7bf37e7deb3b8ecbeff9aca10f5b77c5985f1
Edward Romero <legolamel@gmail.com>
2021-02-14 18:41:59 -0500

    add initial setup

---
+++ src/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  extends: [
+    '@ijsto',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+  },
+};
---
+++ src/components/index.js
@@ -0,0 +1,22 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from './layout';
+import utilStyles from '../styles/utils.module.css';
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>An awesome github actions workflow for deploying to github pages</p>
+        <p>
+          Check out the complete tutorial at <br />
+          <a href="https://www.youtube.com/channel/UCAyEo_24I-oZtsBcAd3Vxig">
+            Meroware Instance Youtube Channel
+          </a>
+        </p>
+      </section>
+    </Layout>
+  );
+}
